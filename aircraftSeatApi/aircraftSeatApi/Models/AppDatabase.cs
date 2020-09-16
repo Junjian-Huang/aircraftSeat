19d84a7e6a3e04cab28c838748afc8b11538703e
Junjian Huang <junjian.huang@hotmail.com>
2020-09-17 02:00:24 +1200

    create base backend project, swagger, database on Azure and set the connection string on it

---
+++ aircraftSeatApi/aircraftSeatApi/Models/AppDatabase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aircraftSeatApi.Models
+{
+    public class AppDatabase : DbContext
+    {
+        public AppDatabase(DbContextOptions<AppDatabase> options) : base(options)
+        {
+
+        }
+
+    }
+}
